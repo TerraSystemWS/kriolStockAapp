98b0ee12ef32d3bd123d1d9616ba59f9db684835
Ailton Duarte <adidas.coyas@gmail.com>
2023-02-06 17:50:20 -0100

    add class counter

--- app/src/main/java/cv/terrasystem/kriolstock/SplashScreen.java
+++ app/src/main/java/cv/terrasystem/kriolstock/SplashScreen.java
@@ -1,14 +1,30 @@
 package cv.terrasystem.kriolstock;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
 
 public class SplashScreen extends AppCompatActivity {
 
+    public static int SPLASH_SCREEN = 5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+                finish();// se nao haver um finish, apos fechar o dash volta a splashScreen
+            }
+        }, SPLASH_SCREEN);
     }
 }
